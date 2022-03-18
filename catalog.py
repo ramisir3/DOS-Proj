ad9d78a6f9016be5dfe820bf314f3d7f11677b9b
waseemghazal10 <waseemghazal10@gmail.com>
2022-03-18 21:51:46 +0200

    info operations ready in catalog

--- catalog.py
+++ catalog.py
@@ -24,3 +24,19 @@ def search(category):
     file.close()
     if flag == 0: s+= "No matching category"
     return s
+    
+@app.route("/info/<item_number>")
+def info(item_number):
+    file = open('catalog.csv')
+    csvreader = csv.reader(file, delimiter=',')
+    s = ""
+    flag = 0
+    for line in csv.DictReader(file):
+    	if line['ID'] == item_number:
+    		flag = 1
+    		line.pop('Category')
+    		line.pop('ID')
+    		s+= json.dumps(line, indent = 4) + "<br>"
+    file.close()
+    if flag == 0: s+= "No matching category"
+    return s
